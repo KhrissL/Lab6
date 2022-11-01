621256ea80c05cc4af3d7edf5a77e5474058922d
KhrissL <christopher.lamm@ufl.edu>
2022-10-31 22:14:42 -0400

    First commit. This is an extremely basic addition function.

---
+++ main.py
@@ -0,0 +1,9 @@
+def addition():
+    # Stupidly basic addition
+    num1 = float(input("Please input a number: "))
+    num2 = float(input("Please input a number: "))
+    result = num1 + num2
+    print(f"Result: {result}")
+
+
+addition()

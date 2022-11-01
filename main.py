ae85fff10f292e1040374c8a45eb13caa7fe5f85
KhrissL <christopher.lamm@ufl.edu>
2022-10-31 22:26:21 -0400

    Replaces addition function with a subtraction function.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def addition():
-    # Stupidly basic addition
+def subtraction():
+    # Stupidly basic subtraction
     num1 = float(input("Please input a number: "))
     num2 = float(input("Please input a number: "))
-    result = num1 + num2
+    result = num1 - num2
     print(f"Result: {result}")
 
 
-addition()
+subtraction()
